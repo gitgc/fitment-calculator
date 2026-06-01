c83ce8de1584a386cf2938de0ac36120572ff8e3
Gio Coia <g@coia.io>
2026-06-01 13:09:19 -0700

    add disclaimer text

--- test/locales.spec.js
+++ test/locales.spec.js
@@ -21,7 +21,7 @@ function escapeRegExp(s) {
 }
 
 const REQUIRED_KEYS = [
-	'lang', 'flag', 'siteTitle', 'pageTitle', 'metaDescription', 'footerText',
+	'lang', 'flag', 'siteTitle', 'pageTitle', 'metaDescription', 'footerText', 'disclaimer',
 	'skipToMain', 'subtitle', 'currentSetup', 'newSetup',
 	'rimSection', 'tireSection', 'optionalSection',
 	'diameterInches', 'widthInches', 'offsetET', 'tireWidth', 'profile', 'spacer', 'camber',
